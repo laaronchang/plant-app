5dfd79c0510fae8e1d8f048d1ce7dad307cff4a3
laaronchang <aacidus@gmail.com>
2024-07-11 12:17:46 -0700

    Added Create action

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -4,4 +4,16 @@ class PlantsController < ApplicationController
     @plants = Plant.all
     render :index
   end
+
+  def create
+    @plant = Plant.create(
+      name: params[:name],
+      species: params[:species],
+      plant_image: params[:plant_image],
+      sun_amount: params[:sun_amount],
+      days_water: params[:days_water],
+      description: params[:description],
+    )
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "/plants" => "plants#index"
+  post "/plants" => "plants#create"
 end
